eed21b90ad032ca1e4302f4023bc496faa237b4c
Uros Tement <ut@scantron.si>
2023-08-17 15:41:26 +0200

    Added gh_token - 6

--- main.py
+++ main.py
@@ -210,7 +210,7 @@ def main() -> bool:
         repo = g.get_repo("velis74/incremental-coverage-check")
         pr = repo.get_pull(11)
         last_commit = pr.get_commits()[pr.commits - 1]
-        comment = pr.create_comment("This is a comment", last_commit, "main.py", 1)
+        comment = pr.create_comment("This is a comment", last_commit, "main.py", 212)
 
         if percentage < args.required_percentage and checked_files_nr > 0:
             logging.info(f"Commit is not covered at least {args.required_percentage}%. Coverage FAILED.")
